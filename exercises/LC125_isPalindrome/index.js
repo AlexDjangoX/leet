e75df1ac109e4fed741867a3a12880dac0bfdd09
AlexDjangoX <alexmonk17@gmail.com>
2022-11-21 11:53:07 +0100

    isPalindrome

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,33 @@
-function isPalindrome(s) {}
+// 0(1) space complexity
 
+function isPalindrome(s) {
+  s = s.toLowerCase().replace(/[\W_]/g, '');
+
+  let indexStart = 0;
+  let indexEnd = s.length - 1;
+
+  while (indexStart < indexEnd) {
+    if (s[indexStart] !== s[indexEnd]) {
+      return false;
+    }
+    indexStart++;
+    indexEnd--;
+  }
+
+  return true;
+}
+
+// o(n) space complexity
+
+const isPalindromeNaive = (s) => {
+  const cleanS = s.toLowerCase().replace(/[\W_]/g, '');
+
+  if (cleanS.split('').reverse().join('') === cleanS) {
+    console.log('Palindrome');
+    return true;
+  }
+  return false;
+};
+
+isPalindromeNaive('A man, a plan, a canal: Panama');
 module.exports = isPalindrome;
